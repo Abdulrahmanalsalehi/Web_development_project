ccf6923a6bf014ca66ad45601776efb9d9178eee
B04-as1807739-abdulrahman <as1807739@qu.edu.qa>
2026-02-28 23:02:10 +0300

    started profile

---
+++ js/user_authentication.js
@@ -0,0 +1,104 @@
+
+// return user or null
+function getUsers(){
+    return JSON.parse(localStorage.getItem("users")) || []; 
+}
+
+// save users in local storage 
+function saveUsers(users){
+    localStorage.setItem("users", JSON.stringify(users));
+
+}
+// check if email is valid 
+function isEmailValid(email){
+    const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    return pattern.test(email);
+
+}
+// check if password is strong
+function isPasswordStrong(password){
+    const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+    return pattern.test(password);
+}
+
+const default_pic = "media/profile-picture.png";
+
+// **** Validate inputs in register ****
+
+// target register form in html doucment
+const register_form = document.querySelector(".register-page form") 
+if(register_form){
+
+    register_form.addEventListener("submit", function(e) {
+        
+        e.preventDefault();
+        
+        // collecting values from user registraion 
+        const fullname = document.getElementById("fullname").value;
+        const username =  document.getElementById("username").value;
+        const email = document.getElementById("email").value;
+        const phone = document.getElementById("phone").value;
+        const password = document.getElementById("password").value;
+        const confirmedpassword = document.getElementById("confirm-password").value;
+       
+        // show aproperiate messages
+        if(!isEmailValid(email)){
+            alert("Invalid email format");
+            return;
+        }
+        if(!isPasswordStrong(password)){
+            alert("Password must be at least 8 characters, include uppercase, lowercase and a number");
+            return;
+        }
+        if(password != confirmedpassword){
+            alert("Passwords do not match");
+            return;
+        }
+        
+        let users = getUsers();
+        
+        // compare email entered with emails on local storage
+        const emailExists = users.some(user => user.email === email);
+        if(emailExists){
+          alert("Email already registered");
+          return;
+        }
+
+        //create an object once validation is passed 
+        const user = {
+            fullname,
+            username,
+            email,
+            phone,
+            password,
+            profilePic: default_pic,
+            bio: "",
+        }
+        users.push(user);
+        saveUsers(users);
+
+        window.location.href = "login.html";
+
+    }); 
+}
+
+// **** Validate inputs in login ****
+
+const login_form = document.querySelector(".login-page form") 
+if(login_form){
+    login_form.addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        const email = document.getElementById("email").value
+        const password = document.getElementById("password").value
+        const users = getUsers();
+
+        const validate_user = users.find(user => user.email == email && user.password == password);
+        if(!validate_user){
+            alert("Invalid email or password");
+            return;
+        }
+        localStorage.setItem("currentUser", JSON.stringify(validate_user));
+
+   });
+}
